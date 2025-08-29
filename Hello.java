a3ff9d4a5b34b6dca820d745dd03fe034d0e1f1c
macys <macys@MacyLaptop.unl.edu>
2025-08-29 14:59:22 -0500

    added my major

---
+++ Hello.java
@@ -0,0 +1,12 @@
+/**
+ * Author: Macy Sweet-Bemer(msweet-bemer@unl.edu) 
+ * Date: 08/29/2025
+ * 
+ * @author msweet-bemer
+ */
+public class Hello {
+
+	public static void main(String args[]) {
+		System.out.println("Macy Sweet-Bemer");;
+	}
+}
